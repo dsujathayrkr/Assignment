896828e6296515e25ecec6bf0fa0f2019a836652
Sujatha D <sujatha.d@ibm.com>
2022-03-16 09:38:10 +0530

    first commit

---
+++ src/test/java/Pojo/pojoclass.java
@@ -0,0 +1,27 @@
+package Pojo;
+
+public class pojoclass {
+	
+	private String employeename;
+	public String getEmployeename() {
+		return employeename;
+	}
+	public void setEmployeename(String employeename) {
+		this.employeename = employeename;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	private String address;
+	private String city;
+
+}
